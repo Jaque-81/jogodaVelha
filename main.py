bbe860e6922db99203835336f61c8ccc3f957129
Jaqueline <59448111+Jaque-81@users.noreply.github.com>
2021-11-29 15:49:35 +0000

    Criando a função tela.

--- main.py
+++ main.py
@@ -14,3 +14,17 @@ velha=[
   [" "," "," "],
   [" "," "," "]
 ]
+def tela ():
+  global velha #indica que eu vou usar a variavel global declarada à cima. 
+  global jogadas
+  os.system("cls")
+  print("    0   1   2")
+  print("0:  "+velha[0][0]+ " | "+velha[0][1]+ " | "+velha[0][2]) 
+  print("   ","-"*11)
+  print("1:  "+velha[1][0]+ " | "+velha[1][1]+ " | "+velha[1][2]) 
+  print("   ","-"*11)
+  print("2:  "+velha[2][0]+ " | "+velha[2][1]+ " | "+velha[2][2]) 
+  print("   ","-"*11)
+  print("Jogadas " + Fore.BLUE + str(jogadas) + Fore.RESET)
+
+tela()
