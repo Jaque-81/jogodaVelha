0a0213e03352ad72f4244db65cd92eb2099d41a6
Jaqueline <59448111+Jaque-81@users.noreply.github.com>
2021-11-29 12:43:43 +0000

    Criando as variáveis globais.

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ from colorama import Fore, Back, Style
 jogar_novamente = "s"
 jogadas=0
 quem_jogada = 2 #um é o pc e o outro o usuário
+max_jogadas = 9
+vit = "n" #verificar vitóriia e pode ser do tipo boolean
+velha=[
+  [" "," "," "],
+  [" "," "," "],
+  [" "," "," "]
+]
