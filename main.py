4cdf2bbc54b09d4ab8e21ba54a04c939fc9b60d3
Jaqueline <59448111+Jaque-81@users.noreply.github.com>
2021-11-29 12:39:43 +0000

    Criação do jogo da velha.

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import os
 import random
 from colorama import Fore, Back, Style
+
+
+#primeira coisa é declarar variáveis globais
+jogar_novamente = "s"
+jogadas=0
+quem_jogada = 2 #um é o pc e o outro o usuário
